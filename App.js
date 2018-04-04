3001d9a289cbfbacc46b3bb4d2e9354c561f6c1e
Olaoluwa Alabi <ayoalabi0@gmail.com>
2018-04-04 09:06:54 +0300

    server.js - main

--- App.js
+++ App.js
@@ -8,6 +8,7 @@ const Menu = require('./api/models/menus');
 //Configure app with express web framework
 const app = express();
 
+
 //implements all other routes
 const menusRoutes = require('./api/routes/menu');
 const usersRoutes = require('./api/routes/users');
@@ -16,7 +17,7 @@ const ordersRoutes = require('./api/routes/orders');
 //connect to mongoDB using mongoose
 mongoose.connect('mongodb://kitan:Olujuwon86~@kitan-shard-00-00-t4x8i.mongodb.net:27017,kitan-shard-00-01-t4x8i.mongodb.net:27017,kitan-shard-00-02-t4x8i.mongodb.net:27017/test?ssl=true&replicaSet=kitan-shard-0&authSource=admin', function() { /* dummy function */ })
 .then(() => {
-    
+    console.error('App started without errors');
 })
 .catch(err => { // mongoose connection error will be handled here
     console.error('App starting error:', err.stack);
--- server.js
+++ server.js
@@ -1,6 +1,34 @@
 //Import/require dependencies
-//const http = require('http');
-const app = require('./App');
+const express = require('express');
+const morgan = require('morgan'); //for logging to console
+const bodyParser = require('body-parser'); //for parsing data to and fro json
+const mongoose = require('mongoose'); // for connecting to MongoDB
+const Menu = require('./api/models/menus');
+const app = express();
+
+const menusRoutes = require('./api/routes/menu');
+const usersRoutes = require('./api/routes/users');
+const ordersRoutes = require('./api/routes/orders');
+
+mongoose.connect('mongodb://kitan:Olujuwon86~@kitan-shard-00-00-t4x8i.mongodb.net:27017,kitan-shard-00-01-t4x8i.mongodb.net:27017,kitan-shard-00-02-t4x8i.mongodb.net:27017/test?ssl=true&replicaSet=kitan-shard-0&authSource=admin', function() { /* dummy function */ })
+.then(() => {
+    console.error('App started without errors');
+})
+.catch(err => { // mongoose connection error will be handled here
+    console.error('App starting error:', err.stack);
+    process.exit(1);
+});
+
+app.use(morgan('dev'));
+
+app.use('/useruploads', express.static('userUploads')); 
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.use('/menus', menusRoutes);
+app.use('/users', usersRoutes);
+app.use('/orders', ordersRoutes);
 
 //create and specify port to use, wit option to use server variable
 const port = process.env.PORT || 9000;
